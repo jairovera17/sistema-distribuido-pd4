ddafd556200fee5e01f6aafa5ede111e74462ad5
Jairo <jairov1995@yahoo.co>
2017-08-17 12:51:17 -0500

    backcommit

--- backEnd/backBoletosFutbol/config/models.js
+++ backEnd/backBoletosFutbol/config/models.js
@@ -21,7 +21,7 @@ module.exports.models = {
 
 
 
-  //connection: 'connectionMongodbServer',
+ // connection: 'connectionMongodbServer',
 
 
   /***************************************************************************
